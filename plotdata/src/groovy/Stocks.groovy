760828b2272be6d6834f9f4d9021c394af25a0a6
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-04 17:49:27 -0400

    works nicely

--- plotdata/src/groovy/Stocks.groovy
+++ plotdata/src/groovy/Stocks.groovy
@@ -3,21 +3,27 @@ import java.net.*
 
   try
         {
-            String[] stocks = ["GOOG", "MSFT"]
-	    // format options for the yahoo finance API: http://www.gummy-stuff.org/Yahoo-data.htm
-            URL yahoofinance = new URL("http://finance.yahoo.com/d/quotes.csv?s=" + stocks[0] + "+" + stocks[1] + "&f=ba"); // bid,ask, e.g. "871.62,872.77"
+            String[] stocks = ["GOOG", "MSFT", "AAPL"]
+	    // format options for the yahoo finance csv API: http://www.gummy-stuff.org/Yahoo-data.htm
+            URL yahoofinance = new URL("http://finance.yahoo.com/d/quotes.csv?s=${stocks.join(",")}&f=ba"); // each line is bid,ask for a stock, e.g. "871.62,872.77"
             URLConnection yc = yahoofinance.openConnection();
-
             
-	    BufferedReader inp = new BufferedReader(new InputStreamReader(
-                    yc.getInputStream()));
+	    BufferedReader inp = new BufferedReader(new InputStreamReader(yc.getInputStream()));
             String inputLine;
-            while ((inputLine = inp.readLine()) != null)
+            def bid_ask_regex = ~/^([\d]+(.[\d]+)?),([\d]+(.[\d]+)?)[ ]*$/    // two comma-separated real numbers
+	    iter = stocks.iterator()
+	    while ((inputLine = inp.readLine()) != null)
             {
-                System.out.println(inputLine);
+                // println inputLine
+		assert inputLine ==~ bid_ask_regex, "Check if this URL correctly uses Yahoo finance csv api to retrieve \"bid,ask\": ${yahoofinance}; stocks:${stocks}"
+		def l = inputLine.split(',')
+		assert iter.hasNext()
+		def stock=iter.next()
+		println "${stock} bid: ${l[0].toDouble()}, ask: ${l[1].toDouble()}"
             }
+	    assert !iter.hasNext()   // should read a line for each stock
             inp.close();
         } catch (IOException ex)
         {
-            System.out.println("Oops bad things happens");
+            println "Something went wrong..."
         }

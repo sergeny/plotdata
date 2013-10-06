6cc330921229b4fd82be87033a8894ca8320f7dd
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-06 00:50:10 -0400

    ...

--- plotdata/scripts/TestMysql.groovy
+++ plotdata/scripts/TestMysql.groovy
@@ -1,4 +1,5 @@
 /* Testing grails/GORM connection to mysql
+ * Call this script "scripts/TestMysql.groovy"
  * Usage: "grails test-mysql" or, to run in another environment, e.g. "grails test test-mysql"
  * (This is in case you have changed DataSource.groovy and nothing is working.)
  *
@@ -7,6 +8,8 @@
  * e.g. grails test test-mysql localhost 3306 test root rootpassword.
  * The database name can also be empty.
  */
+
+import groovy.sql.Sql
 
 includeTargets << grailsScript("_GrailsInit") << grailsScript("_GrailsArgParsing")
 
@@ -23,10 +26,49 @@ target(main: "The description of the script goes here!") {
     println "Database:" + db
     println "User: " + user
     println "Password: " + pswd
-    
+
     def jdbc_string='jdbc:mysql://' + host + ':' + port + '/' + db
 
-    def sql = Sql.newInstance(jdbc_string, user, pswd, "com.mysql.jdbc.Driver")
+    def sql
+    try {
+        sql = Sql.newInstance(jdbc_string, user, pswd, "com.mysql.jdbc.Driver")
+    } catch (com.mysql.jdbc.exceptions.jdbc4.CommunicationsException e) {
+        println "ERROR! Cannot connect to " + host + ":" + port
+        println "Check host, port, your open ports and other firewall settings; try to connect with some other program"
+        println ""
+        println e
+        return
+    } catch (com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException e) {
+        println "MySQL ERROR, perhaps wrong database name!"
+        println ""
+        println e
+        return
+    } catch (java.sql.SQLException e) {
+        println "MySQL ERROR, perhaps wrong login/password!"
+        println ""
+        println e
+        return
+    }
+
+    println "SUCCESS! Connected to MySQL"
+    def query = "SHOW DATABASES"
+
+    println "Executing query " + query + "..."
+    sql.eachRow(query) {
+      println it
+    }
+
+    println "OK, Done!"
+
+    println """
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://${host}:${port}/${db}"
+            username="${user}"
+            password="${pswd}"
+        }
+        """
+
 }
 
 setDefaultTarget(main)

5510c5a12dc6fb337fbf577e2e1c65bc16bdb0e1
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-07 21:42:58 -0400

    TimeSeries and StockSeries

---
+++ plotdata/grails-app/domain/com/sergeyorshanskiy/domain/StockSeries.groovy
@@ -0,0 +1,14 @@
+package com.sergeyorshanskiy.domain
+
+
+// Stock price information for a particular ticker.
+class StockSeries extends TimeSeries {
+
+	String ticker; // e.g. "AAPL"
+
+	// We are going to pick a series by ticker, so it better be unique. 
+	// E.g. all Apple stock prices will be contained in just one StockSeries.
+    static constraints = {
+		ticker(unique: true)
+    }
+}
---
+++ plotdata/test/unit/com/sergeyorshanskiy/domain/TimeSeriesTests.groovy
@@ -0,0 +1,17 @@
+package com.sergeyorshanskiy.domain
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TimeSeries)
+class TimeSeriesTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}

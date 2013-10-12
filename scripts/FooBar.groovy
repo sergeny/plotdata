ebb99ab6fff0bd638e42af7123c32a8867fcfcab
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-12 19:55:16 -0400

    removed files with experimental code, no longer needed

--- scripts/FooBar.groovy
+++
@@ -1,8 +0,0 @@
-includeTargets << grailsScript("_GrailsInit")
-
-target(main: "Runs a generic script and passes parameters") {
-  def myclass = classLoader.loadClass('GenericRunScript')
-  myclass.execute(args);
-}
-
-setDefaultTarget(main)
--- scripts/Test.groovy
+++
@@ -1,9 +0,0 @@
-import com.sergeyorshanskiy.domain.DataPoint
-
-includeTargets << grailsScript("_GrailsInit")
-
-target(main: "The description of the script goes here!") {
-    // TODO: Implement script here
-}
-
-setDefaultTarget(main)

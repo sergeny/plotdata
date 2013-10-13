e92b2772f110a24285e2832ce8f5e900dc49de0b
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-12 20:56:35 -0400

    now passing to the task info about data to update

--- src/groovy/BackendTask.groovy
+++ src/groovy/BackendTask.groovy
@@ -5,31 +5,41 @@ import java.util.concurrent.TimeUnit
 
 
 // BackendTask.groovy and BackendWatchdog.groovy
-// This code was essentially taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
+// This code was originally taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
 // Also take a look at the follow-up post, periodic task+watchdog+generics, http://blog.temposwc.com/2011/01/periodic-task-and-watchdog-reusable-and.html
 // This code's copyright status is unknown (which is why I am reusing the simpler version --- it is so simple that it would be almost certainly considered common knowledge.)
 
 
+
+
 public class BackendTask {
- 
+	def SERIES_TABLE='series'
+	def POINTS_TABLE='points' 
+
 	private int period // Execute every that many milliseconds
-    private int total, startTotal;
+    private def update // Data to update - probably a dictionary with dictionaries of groovy closures
+	private int total, startTotal;
     private ScheduledExecutorService executor;
     private ScheduledFuture future;
  
-    public BackendTask() {
-        this(1000);
+    public BackendTask(def dataToUpdate) {
+        this(dataToUpdate, 1000);
     }
  
     /**
      * Construct the task with the given total - useful to restore its state as needed.
      */
-    public BackendTask(int period) {
+    public BackendTask(def dataToUpdate, int period) {
 		this.period = period
+		update = dataToUpdate
         total = 0;
         startTotal = total;
         start();
     }
+
+	public getDataToUpdate() {
+		return update;
+	}
 
 	public getPeriod() {
 		return period;
@@ -62,11 +72,43 @@ public class BackendTask {
     public void shutdown() {
         executor.shutdown();
     }
- 
+  
+	// e.g. type is "local", name is "cpu" or 
+	// type is "stock", name is "AAPL"
+	private int seriesId(String seriesType, String seriesName) {
+		return 1
+	}
+
     private class TheTask implements Runnable {	
         public void run() {
-        	println "Running..."
-    		// update object state on each execution
+        	println "Running... "
+    	
+			
+			// First compute all the new values, then put all of them to a database
+			// Currently using a separate timestamp for each value
+			// May be a waste. But then, if one update takes forever (network connection, etc.),
+			// the other timestamps will be more accurate.
+			def new_values=[:]
+		    if (update["local"]) {
+				new_values["local"] = [:]
+				def l = new_values["local"]
+		        update["local"].each { 
+		            name, v_closure -> //println "${k}, ${v()}"
+						l[name] = [new Date().getTime(), v_closure()] // Execute the closure simultaneously with taking the timestamp
+		        }
+		    }
+		
+			println "${new_values}"
+			
+			new_values.each {
+				s_type, d -> d.each {
+					name, p ->  // p[0] - timestamp, p[1] - value
+						println "INSERT ${SERIES_TABLE}_id=${seriesId(s_type, name)} " +			
+						"${p}"//	"time=${l[0]} value=${l[1]}"
+				}
+			}
+		
+			// update object state on each execution
             total++;
             if (total > startTotal + 10) {
                 // generate an exception to simulate what might happen
--- src/groovy/BackendWatchdog.groovy
+++ src/groovy/BackendWatchdog.groovy
@@ -3,7 +3,7 @@ import java.util.concurrent.TimeUnit
 
 
 // BackendTask.groovy and BackendWatchdog.groovy
-// This code was essentially taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
+// This code was originally taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
 // Also take a look at the follow-up post, periodic task+watchdog+generics, http://blog.temposwc.com/2011/01/periodic-task-and-watchdog-reusable-and.html
 // This code's copyright status is unknown (which is why I am reusing the simpler version --- it is so simple that it would be almost certainly considered common knowledge.)
 
@@ -15,7 +15,7 @@ public class BackendWatchdog {
 
     public BackendWatchdog(BackendTask theTask) {
         task = theTask;
-        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Watcher(), 0, 200, TimeUnit.MILLISECONDS);
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Watcher(), 0, 2000, TimeUnit.MILLISECONDS);
     }
  
     public BackendTask getTask() {
@@ -25,18 +25,18 @@ public class BackendWatchdog {
     private class Watcher implements Runnable {
  
         public void run() {
- 
-            while (true) {
+            //while (true) {
                 if (task.isDone()) {
                     System.out.println("Task stopped - restarting...");
                     //  cancel the task and shutdown the executor
                     task.cancel(true);
                     task.shutdown();
                     // restart from where we left off - state gets restored:
+					def update = task.getDataToUpdate()
                     int period = task.getPeriod();
-                    task = new BackendTask(period);
+                    task = new BackendTask(update, period);
                 }
-            }
+            //}
         }
     }
 }

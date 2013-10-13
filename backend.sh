3e3c47f100efbf6900bca7b02e2675c40f64f705
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-12 20:00:44 -0400

    Now can pass a -c argument (config file) to backend.sh, in case you don't want to use BackendConfig_development.groovy

--- backend.sh
+++ backend.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-groovy -classpath src/groovy/ src/groovy/Backend.groovy -c BackendConfig_development.groovy $*
+# Note that $* is before -c because so that the user can pass another config file to this script
+# Backend.groovy will use the first -c argument specified, not the last one
+groovy -classpath src/groovy/ src/groovy/Backend.groovy $* -c BackendConfig_development.groovy

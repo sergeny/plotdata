9dde83014faeb7957812956e74ce187f4c3cf0e1
Sergey Orshanskiy <orshansk@gmail.com>
2013-10-16 00:43:24 -0400

    comments

--- src/groovy/BackendWatchdog.groovy
+++ src/groovy/BackendWatchdog.groovy
@@ -1,11 +1,12 @@
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-
-// BackendTask.groovy and BackendWatchdog.groovy
-// This code was originally taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
-// Also take a look at the follow-up post, periodic task+watchdog+generics, http://blog.temposwc.com/2011/01/periodic-task-and-watchdog-reusable-and.html
-// This code's copyright status is unknown (which is why I am reusing the simpler version --- it is so simple that it would be almost certainly considered common knowledge.)
+/*
+ * The files BackendTask.groovy and BackendWatchdog.groovy implement a design pattern: a "Watchdog" watching over a "Task" run by an Executor.
+ * The implementation of the design pattern was taken from the blog http://blog.temposwc.com/2011/01/schedule-periodic-task-with-watchdog.html
+ * Also take a look at the follow-up post, periodic task+watchdog+generics, http://blog.temposwc.com/2011/01/periodic-task-and-watchdog-reusable-and.html
+ *
+ */
 
 
 public class BackendWatchdog {
@@ -15,6 +16,7 @@ public class BackendWatchdog {
 
     public BackendWatchdog(BackendTask theTask) {
         task = theTask;
+		// Watching every 2 seconds. TODO: do we need to watch at all, and what would be the best period?
         Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Watcher(), 0, 2000, TimeUnit.MILLISECONDS);
     }
  
